3efe8f6257a0cb3148acce0c9333dd3fead28e94
blead <blead010@gmail.com>
2016-12-10 03:57:18 +0700

    reset velocity after updating

--- src/systems/MovementSystem.java
+++ src/systems/MovementSystem.java
@@ -5,6 +5,7 @@ import components.VelocityComponent;
 import core.Entity;
 import core.EntityManager;
 import core.EntitySystem;
+import javafx.geometry.Point2D;
 import utils.ComponentNotFoundException;
 
 public class MovementSystem extends EntitySystem {
@@ -15,6 +16,7 @@ public class MovementSystem extends EntitySystem {
 				PositionComponent positionComponent = (PositionComponent) entity.getComponent(PositionComponent.class);
 				VelocityComponent velocityComponent = (VelocityComponent) entity.getComponent(VelocityComponent.class);
 				positionComponent.setPosition(positionComponent.getPosition().add(velocityComponent.getVelocity()));
+				velocityComponent.setVelocity(Point2D.ZERO);
 				/* TODO: .multiply(deltaTime * UNIT_PER_TIME); */
 			} catch (ComponentNotFoundException e) {
 				continue;

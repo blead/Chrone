2bc59016777ba7247d2fed58480357787fd66389
blead <blead010@gmail.com>
2016-12-11 01:10:30 +0700

    create Main, CameraSystem

---
+++ src/main/Main.java
@@ -0,0 +1,117 @@
+package main;
+
+import core.EntitySystemManager;
+import core.Level;
+import core.LevelManager;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import systems.CameraSystem;
+import systems.CollisionSystem;
+import systems.GravitySystem;
+import systems.MovementSystem;
+import systems.RenderSystem;
+
+public class Main extends Application {
+	public static final String TITLE = "Chrone";
+	public static final boolean IS_RESIZABLE = false;
+	public static final int WIDTH = 1600;
+	public static final int HEIGHT = 900;
+	public static Main instance = null;
+	private Pane applicationRoot;
+	private Canvas gameRoot;
+	private Thread gameLoop;
+	private boolean isRunning;
+
+	public Main() {
+		synchronized (Main.class) {
+			if (instance != null)
+				throw new UnsupportedOperationException();
+			instance = this;
+		}
+	}
+
+	public static Main getInstance() {
+		return instance;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public Pane getApplicationRoot() {
+		return applicationRoot;
+	}
+
+	public void setApplicationRoot(Pane applicationRoot) {
+		this.applicationRoot = applicationRoot;
+	}
+
+	public Canvas getGameRoot() {
+		return gameRoot;
+	}
+
+	public void setGameRoot(Canvas gameRoot) {
+		this.gameRoot = gameRoot;
+	}
+
+	public boolean isRunning() {
+		return isRunning;
+	}
+
+	public void setRunning(boolean isRunning) {
+		this.isRunning = isRunning;
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		LevelManager.getInstance().setLevel(new Level(LevelManager.LEVEL_ONE));
+		LevelManager.getInstance().load();
+		applicationRoot = new Pane();
+		applicationRoot.setPrefSize(Main.WIDTH, Main.HEIGHT);
+		applicationRoot.getChildren().add(gameRoot);
+		primaryStage.setTitle(Main.TITLE);
+		primaryStage.setResizable(Main.IS_RESIZABLE);
+		primaryStage.setScene(new Scene(applicationRoot));
+		primaryStage.show();
+		isRunning = true;
+
+		EntitySystemManager.getInstance().add(new GravitySystem(), new CollisionSystem(), new MovementSystem(),
+				new CameraSystem(), new RenderSystem());
+		gameLoop = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				final int FPS = 60;
+				final long timePerFrame = 1000000000 / FPS;
+				long lastUpdateTime = System.nanoTime();
+				while (Main.getInstance().isRunning()) {
+					long now = System.nanoTime();
+					double deltaTime = (now - lastUpdateTime) / timePerFrame;
+					// deltaTime in frame unit
+					if (deltaTime >= 1) {
+						EntitySystemManager.getInstance().update(deltaTime);
+						lastUpdateTime = now;
+					}
+					long timeUntilNextUpdate = (lastUpdateTime + timePerFrame - System.nanoTime()) / 1000000;
+					if (timeUntilNextUpdate > 0) {
+						try {
+							Thread.sleep(timeUntilNextUpdate);
+						} catch (InterruptedException e) {
+							break;
+						}
+					}
+				}
+			}
+		});
+		gameLoop.start();
+	}
+
+	@Override
+	public void stop() throws Exception {
+		setRunning(false);
+		gameLoop.interrupt();
+		super.stop();
+	}
+}
---
+++ src/systems/CameraSystem.java
@@ -0,0 +1,102 @@
+package systems;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+import components.CameraComponent;
+import components.PositionComponent;
+import components.RenderComponent;
+import core.Entity;
+import core.EntityManager;
+import core.EntitySystem;
+import core.LevelManager;
+import javafx.application.Platform;
+import javafx.geometry.Point2D;
+import main.Main;
+import utils.ComponentNotFoundException;
+import utils.PrioritizedPoint2D;
+import utils.Renderable;
+
+public class CameraSystem extends EntitySystem {
+	public CameraSystem() {
+		super(50);
+	}
+
+	@Override
+	public void update(double deltaTime) {
+		Queue<PrioritizedPoint2D> anchorPositions = new PriorityQueue<>();
+		for (Entity entity : EntityManager.getInstance().getEntities()) {
+			try {
+				RenderComponent renderComponent = (RenderComponent) entity.getComponent(RenderComponent.class);
+				PositionComponent positionComponent = (PositionComponent) entity.getComponent(PositionComponent.class);
+				CameraComponent cameraComponent = (CameraComponent) entity.getComponent(CameraComponent.class);
+				anchorPositions.add(new PrioritizedPoint2D(getAnchorPosition(renderComponent.getShape(),
+						renderComponent.getOffset(), positionComponent.getPosition()), cameraComponent.getPriority()));
+			} catch (ComponentNotFoundException e) {
+				continue;
+			}
+		}
+		Point2D viewportPosition = getAdjustedViewportPosition(
+				getViewportPosition(anchorPositions, Main.WIDTH, Main.HEIGHT), Main.WIDTH, Main.HEIGHT,
+				LevelManager.getInstance().getLevel().getWidth(), LevelManager.getInstance().getLevel().getHeight());
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				Main.getInstance().getGameRoot().setTranslateX(-viewportPosition.getX());
+				Main.getInstance().getGameRoot().setTranslateY(-viewportPosition.getY());
+			}
+		});
+	}
+
+	private Point2D getAnchorPosition(Renderable shape, Point2D offset, Point2D position) {
+		return position.add(offset).add(shape.getWidth() / 2, shape.getHeight() / 2);
+	}
+
+	private Point2D getViewportPosition(Queue<PrioritizedPoint2D> anchorPositions, double viewportWidth,
+			double viewportHeight) {
+		if (anchorPositions.size() > 0) {
+			Point2D minPosition = new Point2D(Double.MAX_VALUE, Double.MAX_VALUE),
+					maxPosition = new Point2D(Double.MIN_VALUE, Double.MIN_VALUE);
+			while (!anchorPositions.isEmpty()) {
+				Point2D newPosition = anchorPositions.poll().getPoint2D(),
+						newMinPosition = new Point2D(Math.min(minPosition.getX(), newPosition.getX()),
+								Math.min(minPosition.getY(), newPosition.getY())),
+						newMaxPosition = new Point2D(Math.max(maxPosition.getX(), newPosition.getX()),
+								Math.max(maxPosition.getY(), newPosition.getY()));
+				if (newMaxPosition.getX() - newMinPosition.getX() < viewportWidth
+						&& newMaxPosition.getY() - newMinPosition.getY() < viewportHeight) {
+					minPosition = newMinPosition;
+					maxPosition = newMaxPosition;
+				} else {
+					break;
+				}
+			}
+			Point2D offset = new Point2D(viewportWidth - (maxPosition.getX() - minPosition.getX()) / 2,
+					(viewportHeight - (maxPosition.getY() - minPosition.getY())) / 2);
+			return minPosition.subtract(offset);
+		} else {
+			return Point2D.ZERO;
+		}
+	}
+
+	private Point2D getAdjustedViewportPosition(Point2D viewportPosition, double viewportWidth, double viewportHeight,
+			double levelWidth, double levelHeight) {
+		if (viewportWidth > levelWidth) {
+			viewportPosition = viewportPosition.subtract((viewportWidth - levelWidth) / 2, 0);
+		} else {
+			if (viewportPosition.getX() < 0)
+				viewportPosition = viewportPosition.subtract(viewportPosition.getX(), 0);
+			if (viewportPosition.getX() + viewportWidth > levelWidth)
+				viewportPosition = viewportPosition.subtract(viewportPosition.getX() + viewportWidth - levelWidth, 0);
+		}
+		if (viewportHeight > levelHeight) {
+			viewportPosition = viewportPosition.subtract(0, (viewportHeight - levelHeight) / 2);
+		} else {
+			if (viewportPosition.getY() < 0)
+				viewportPosition = viewportPosition.subtract(0, viewportPosition.getY());
+			if (viewportPosition.getY() + viewportHeight > levelHeight)
+				viewportPosition = viewportPosition.subtract(0, viewportPosition.getY() + viewportHeight - levelHeight);
+		}
+		return viewportPosition;
+	}
+}

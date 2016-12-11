63a6e96d082a457afb646edd89fd89dad07b46ef
blead <blead010@gmail.com>
2016-12-12 01:29:05 +0700

    add Gson library, add DEFAULT_GRAVITY constant and gravity value to Level class, implement Level.fromJson() and toJson()

--- src/core/Level.java
+++ src/core/Level.java
@@ -1,14 +1,35 @@
 package core;
 
+import com.google.gson.Gson;
+
+import javafx.geometry.Point2D;
+
 public class Level {
 	public static final double TILE_SIZE = 60;
+	public static final Point2D DEFAULT_GRAVITY = new Point2D(0, 6);
 	private final String[] data;
 	private final double width, height;
+	private Point2D gravity;
 
 	public Level(String[] data) {
+		this(data, Level.DEFAULT_GRAVITY);
+	}
+
+	public Level(String[] data, Point2D gravity) {
 		this.data = data;
 		width = data[0].length() * Level.TILE_SIZE;
 		height = data.length * Level.TILE_SIZE;
+		this.gravity = gravity;
+	}
+
+	public static Level fromJson(String json) {
+		Gson gson = new Gson();
+		return gson.fromJson(json, Level.class);
+	}
+
+	public String toJson() {
+		Gson gson = new Gson();
+		return gson.toJson(this);
 	}
 
 	public String[] getData() {
@@ -21,5 +42,13 @@ public class Level {
 
 	public double getHeight() {
 		return height;
+	}
+
+	public Point2D getGravity() {
+		return gravity;
+	}
+
+	public void setGravity(Point2D gravity) {
+		this.gravity = gravity;
 	}
 }
